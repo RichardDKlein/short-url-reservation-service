428b5c1c2fc52873f98b9ff8e55179950b2ccc4e
RichardDKlein <RichardDKlein@gmail.com>
2024-01-03 10:38:26 -0600

    Refactoring

--- src/main/java/com/richarddklein/shorturlreservationservice/controller/ShortUrlReservationController.java
+++ src/main/java/com/richarddklein/shorturlreservationservice/controller/ShortUrlReservationController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/shorturl/reservations")
+@RequestMapping({"/shorturl/reservations", "/"})
 public class ShortUrlReservationController {
     private final ShortUrlReservationService shortUrlReservationService;
 

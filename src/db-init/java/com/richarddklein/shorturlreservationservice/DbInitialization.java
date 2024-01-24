e71078f8f51ca690a9b25a3aa011a2d2b3305ed1
RichardDKlein <RichardDKlein@gmail.com>
2024-01-23 19:20:57 -0600

    Refactoring

---
+++ src/db-init/java/com/richarddklein/shorturlreservationservice/DbInitialization.java
@@ -0,0 +1,11 @@
+package com.richarddklein.shorturlreservationservice;
+
+public class DbInitialization {
+    public static void main(String[] args) {
+        initializeDb();
+    }
+
+    private static void initializeDb() {
+        System.out.println("Hello world!");
+    }
+}
